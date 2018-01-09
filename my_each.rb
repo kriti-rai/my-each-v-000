c82eabb42c2002818f28a5a766e9a41f836fc9de
Kriti Rai <kriti.rai@gmail.com>
2018-01-09 17:18:38 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) 
+  
+	index = 0
+  while array[index]
+    yield(array[index])
+    index +=1
+  end
+  array
 end
+
+my_each(array) {|i| puts i}
